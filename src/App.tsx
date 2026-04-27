27c7ba51787e092c6ce3bf978ee895fc2b2b4dcb
Hassan-ureshi <hassanqureshi8886@gmail.com>
2026-04-27 04:50:35 -0700

    refactor: update App, Home page, global styles, and main entry

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import { useState, useEffect } from "react";
 import { AnimatePresence } from "framer-motion";
+import { Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import Loader from "./components/Loader";
+
+// Service Components
+import DigitalMarketing from "./components/Services/DigitalMarketing";
+import GraphicDesigning from "./components/Services/GraphicDesigning";
+import SmBranding from "./components/Services/SmBranding";
+import VideoEditing from "./components/Services/VideoEditing";
+import WebDevelopment from "./components/Services/WebDevelopment";
 
 const App = () => {
   const [isLoading, setIsLoading] = useState(true);
@@ -19,7 +27,14 @@ const App = () => {
       <AnimatePresence>
         {isLoading && <Loader onComplete={() => setIsLoading(false)} />}
       </AnimatePresence>
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/services/digital-marketing" element={<DigitalMarketing />} />
+        <Route path="/services/graphic-designing" element={<GraphicDesigning />} />
+        <Route path="/services/sm-branding" element={<SmBranding />} />
+        <Route path="/services/video-editing" element={<VideoEditing />} />
+        <Route path="/services/web-development" element={<WebDevelopment />} />
+      </Routes>
     </div>
   );
 };
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -3,7 +3,9 @@ import Hero from "../components/Hero"
 import About from "../components/About"
 import Stats from "../components/Stats"
 import OurProcess from "../components/OurProcess"
-import Testimonials from "../components/Testimonials"
+import Services from "../components/Services"
+import Testimonials from "../components/Testimonials" 
+import Brands from "../components/Brands"
 import Team from "../components/Team"
 import Faqs from "../components/Faqs"
 import Footer from "../components/Footer"
@@ -12,14 +14,16 @@ const Home = () => {
   return (
     <div>
       <Navbar/>
-      <Hero/>
+      <div id="home"><Hero/></div>
       <Stats/>
-      <About/>
-      <OurProcess/>
-      <Testimonials/>
-      <Team/>
-      <Faqs/>
-      <Footer/>
+      <div id="about"><About/></div>
+      <div id="process"><OurProcess/></div>
+      <div id="services"><Services/></div>
+      <div id="testimonials"><Testimonials/></div>
+      <Brands/>
+      <div id="team"><Team/></div>
+      <div id="faqs"><Faqs/></div>
+      <div id="contact"><Footer/></div>
     </div>
   )
 }
